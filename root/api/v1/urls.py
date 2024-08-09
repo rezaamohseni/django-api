a1036193d2d7269e40e789c37407af7cf25804da
rezamohseni <rezamohseni@gmail.com>
2024-08-10 00:27:23 +0330

    api root compelete

--- root/api/v1/urls.py
+++ root/api/v1/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     
     path('service' ,  ServiceApiViewSet.as_view({'get':'list' , 'post':'create', 'patch':'update' , 'delete':'destroy'}), name='services'),
     path('specialservice' ,  SpecialServiceApiViewSet.as_view({'get':'list' , 'post':'create', 'patch':'update' , 'delete':'destroy'}) , name='specialservice'),
-    path('team' ,  TeamApiViewSet.as_view({'get':'list', 'patch':'update' , 'delete':'destroy'}), name='team'),
-    path('testimonial' ,  TestimonialsApiViewSet.as_view({'get':'list','post':'create', 'patch':'update' , 'delete':'destroy'}), name='testimonial'),
+    path('team' ,  TeamApiViewSet.as_view({'get':'list', 'delete':'destroy'}), name='team'),
+    path('testimonial' ,  TestimonialsApiViewSet.as_view({'get':'list','delete':'destroy'}), name='testimonial'),
     path('skill' , SkillApiViewSet.as_view({'get':'list','post':'create', 'patch':'update' , 'delete':'destroy'}), name='skill'),
-    path('contact' ,  ContactUsApiViewSet.as_view({'get':'list' ,'post':'create'}), name='contactus'),
+    path('contact' ,  ContactUsApiViewSet.as_view({'post':'create'}), name='contactus'),
 ]
 
